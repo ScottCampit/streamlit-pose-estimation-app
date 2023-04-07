2cd0092f77476c17f531b3c526f9704e39e35cfb
ScottCampit <scottcampit@gmail.com>
2023-04-06 20:07:39 -0700

    Enabled video annotation to work. Need to integrate to streamlit

--- dev-app.py
+++ dev-app.py
@@ -62,133 +62,6 @@ KEYPOINT_EDGE_INDS_TO_COLOR = {
     (14, 16): 'c'
 }
 
-def _keypoints_and_edges_for_display(keypoints_with_scores,
-                                     height,
-                                     width,
-                                     keypoint_threshold=0.11):
-  """Returns high confidence keypoints and edges for visualization.
-
-  Args:
-    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
-      the keypoint coordinates and scores returned from the MoveNet model.
-    height: height of the image in pixels.
-    width: width of the image in pixels.
-    keypoint_threshold: minimum confidence score for a keypoint to be
-      visualized.
-
-  Returns:
-    A (keypoints_xy, edges_xy, edge_colors) containing:
-      * the coordinates of all keypoints of all detected entities;
-      * the coordinates of all skeleton edges of all detected entities;
-      * the colors in which the edges should be plotted.
-  """
-  keypoints_all = []
-  keypoint_edges_all = []
-  edge_colors = []
-  num_instances, _, _, _ = keypoints_with_scores.shape
-  for idx in range(num_instances):
-    kpts_x = keypoints_with_scores[0, idx, :, 1]
-    kpts_y = keypoints_with_scores[0, idx, :, 0]
-    kpts_scores = keypoints_with_scores[0, idx, :, 2]
-    kpts_absolute_xy = np.stack(
-        [width * np.array(kpts_x), height * np.array(kpts_y)], axis=-1)
-    kpts_above_thresh_absolute = kpts_absolute_xy[
-        kpts_scores > keypoint_threshold, :]
-    keypoints_all.append(kpts_above_thresh_absolute)
-
-    for edge_pair, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
-      if (kpts_scores[edge_pair[0]] > keypoint_threshold and
-          kpts_scores[edge_pair[1]] > keypoint_threshold):
-        x_start = kpts_absolute_xy[edge_pair[0], 0]
-        y_start = kpts_absolute_xy[edge_pair[0], 1]
-        x_end = kpts_absolute_xy[edge_pair[1], 0]
-        y_end = kpts_absolute_xy[edge_pair[1], 1]
-        line_seg = np.array([[x_start, y_start], [x_end, y_end]])
-        keypoint_edges_all.append(line_seg)
-        edge_colors.append(color)
-  if keypoints_all:
-    keypoints_xy = np.concatenate(keypoints_all, axis=0)
-  else:
-    keypoints_xy = np.zeros((0, 17, 2))
-
-  if keypoint_edges_all:
-    edges_xy = np.stack(keypoint_edges_all, axis=0)
-  else:
-    edges_xy = np.zeros((0, 2, 2))
-  return keypoints_xy, edges_xy, edge_colors
-
-
-def draw_prediction_on_image(
-    image, keypoints_with_scores, crop_region=None, close_figure=False,
-    output_image_height=None):
-  """Draws the keypoint predictions on image.
-
-  Args:
-    image: A numpy array with shape [height, width, channel] representing the
-      pixel values of the input image.
-    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
-      the keypoint coordinates and scores returned from the MoveNet model.
-    crop_region: A dictionary that defines the coordinates of the bounding box
-      of the crop region in normalized coordinates (see the init_crop_region
-      function below for more detail). If provided, this function will also
-      draw the bounding box on the image.
-    output_image_height: An integer indicating the height of the output image.
-      Note that the image aspect ratio will be the same as the input image.
-
-  Returns:
-    A numpy array with shape [out_height, out_width, channel] representing the
-    image overlaid with keypoint predictions.
-  """
-  height, width, channel = image.shape
-  aspect_ratio = float(width) / height
-  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
-  # To remove the huge white borders
-  fig.tight_layout(pad=0)
-  ax.margins(0)
-  ax.set_yticklabels([])
-  ax.set_xticklabels([])
-  plt.axis('off')
-
-  im = ax.imshow(image)
-  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
-  ax.add_collection(line_segments)
-  # Turn off tick labels
-  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
-
-  (keypoint_locs, keypoint_edges,
-   edge_colors) = _keypoints_and_edges_for_display(
-       keypoints_with_scores, height, width)
-
-  line_segments.set_segments(keypoint_edges)
-  line_segments.set_color(edge_colors)
-  if keypoint_edges.shape[0]:
-    line_segments.set_segments(keypoint_edges)
-    line_segments.set_color(edge_colors)
-  if keypoint_locs.shape[0]:
-    scat.set_offsets(keypoint_locs)
-
-  if crop_region is not None:
-    xmin = max(crop_region['x_min'] * width, 0.0)
-    ymin = max(crop_region['y_min'] * height, 0.0)
-    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
-    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
-    rect = patches.Rectangle(
-        (xmin,ymin),rec_width,rec_height,
-        linewidth=1,edgecolor='b',facecolor='none')
-    ax.add_patch(rect)
-
-  fig.canvas.draw()
-  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-  image_from_plot = image_from_plot.reshape(
-      fig.canvas.get_width_height()[::-1] + (3,))
-  plt.close(fig)
-  if output_image_height is not None:
-    output_image_width = int(output_image_height / height * width)
-    image_from_plot = cv2.resize(
-        image_from_plot, dsize=(output_image_width, output_image_height),
-         interpolation=cv2.INTER_CUBIC)
-  return image_from_plot
-
 def load_local_test_image():
   """Load a local test image"""
   image_path = "/mnt/c/Users/owner/Software/couro-models/data/A2D/RunClips/_aJOs5B9T-Q/00141.png"
@@ -232,30 +105,6 @@ def movenet_predict(interpreter, x):
   interpreter.invoke()
   keypoints = interpreter.get_tensor(output_details[0]['index'])
   return keypoints
-
-def visualize_keypoints(image, keypoints, dim_size=256):
-  """Visualizes the keypoints on the image."""
-  display_image = tf.expand_dims(image, axis=0)
-  display_image = tf.cast(tf.image.resize_with_pad(
-      display_image, dim_size, dim_size), dtype=tf.int32)
-  output_overlay = draw_prediction_on_image(np.squeeze(display_image.numpy(), axis=0), keypoints)
-  plt.figure(figsize=(5, 5))
-  plt.imshow(output_overlay)
-  plt.savefig(os.path.join('./test/images/', datetime.now().strftime('%Y%m-%d%H-%M%S')))
-  _ = plt.axis('off')
-
-def visualize_movenet_models(model_list:list, input_sizes:list):
-  """"""
-  base = "/mnt/c/Users/owner/Software/couro-models/base/"
-  for idx, model_path in enumerate(model_list):
-    dim_size = input_sizes[idx]
-    image = load_local_test_image()
-    x = resize_image(image, dim_size=dim_size)
-    image = tf.image.resize_with_pad(image, dim_size, dim_size)
-    
-    model = load_movenet_model(os.path.join(base, model_path))
-    keypoints = movenet_predict(model, x)
-    #visualize_keypoints(image, keypoints)
 
 def calculate_angle(proximal:float, medial:float, distal:float):
   """Calculate the angle given 3 keypoints (proxima, medial, and distal)."""
@@ -316,30 +165,186 @@ def calculate_2D_joint_angles(keypoints:np.ndarray):
   right_angle = (right_angle_shoulder, right_angle_knee, right_angle_hip)
   return left_angle, right_angle
 
-def movenet_video_inference(mp4_path:str):
+def _keypoints_and_edges_for_display(keypoints_with_scores,
+                                     height,
+                                     width,
+                                     keypoint_threshold=0.11):
+  """Returns high confidence keypoints and edges for visualization.
+
+  Args:
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    height: height of the image in pixels.
+    width: width of the image in pixels.
+    keypoint_threshold: minimum confidence score for a keypoint to be
+      visualized.
+
+  Returns:
+    A (keypoints_xy, edges_xy, edge_colors) containing:
+      * the coordinates of all keypoints of all detected entities;
+      * the coordinates of all skeleton edges of all detected entities;
+      * the colors in which the edges should be plotted.
+  """
+  keypoints_all = []
+  keypoint_edges_all = []
+  edge_colors = []
+  num_instances, _, _, _ = keypoints_with_scores.shape
+  for idx in range(num_instances):
+    kpts_x = keypoints_with_scores[0, idx, :, 1]
+    kpts_y = keypoints_with_scores[0, idx, :, 0]
+    kpts_scores = keypoints_with_scores[0, idx, :, 2]
+    kpts_absolute_xy = np.stack(
+        [width * np.array(kpts_x), height * np.array(kpts_y)], axis=-1)
+    kpts_above_thresh_absolute = kpts_absolute_xy[
+        kpts_scores > keypoint_threshold, :]
+    keypoints_all.append(kpts_above_thresh_absolute)
+
+    for edge_pair, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
+      if (kpts_scores[edge_pair[0]] > keypoint_threshold and
+          kpts_scores[edge_pair[1]] > keypoint_threshold):
+        x_start = kpts_absolute_xy[edge_pair[0], 0]
+        y_start = kpts_absolute_xy[edge_pair[0], 1]
+        x_end = kpts_absolute_xy[edge_pair[1], 0]
+        y_end = kpts_absolute_xy[edge_pair[1], 1]
+        line_seg = np.array([[x_start, y_start], [x_end, y_end]])
+        keypoint_edges_all.append(line_seg)
+        edge_colors.append(color)
+  if keypoints_all:
+    keypoints_xy = np.concatenate(keypoints_all, axis=0)
+  else:
+    keypoints_xy = np.zeros((0, 17, 2))
+
+  if keypoint_edges_all:
+    edges_xy = np.stack(keypoint_edges_all, axis=0)
+  else:
+    edges_xy = np.zeros((0, 2, 2))
+  return keypoints_xy, edges_xy, edge_colors
+
+def draw_prediction_on_image(
+    image, keypoints_with_scores, crop_region=None, close_figure=False,
+    output_image_height=None):
+  """Draws the keypoint predictions on image.
+
+  Args:
+    image: A numpy array with shape [height, width, channel] representing the
+      pixel values of the input image.
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    crop_region: A dictionary that defines the coordinates of the bounding box
+      of the crop region in normalized coordinates (see the init_crop_region
+      function below for more detail). If provided, this function will also
+      draw the bounding box on the image.
+    output_image_height: An integer indicating the height of the output image.
+      Note that the image aspect ratio will be the same as the input image.
+
+  Returns:
+    A numpy array with shape [out_height, out_width, channel] representing the
+    image overlaid with keypoint predictions.
+  """
+  height, width, channel = image.shape
+  aspect_ratio = float(width) / height
+  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
+  # To remove the huge white borders
+  fig.tight_layout(pad=0)
+  ax.margins(0)
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+  plt.axis('off')
+
+  im = ax.imshow(image)
+  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
+  ax.add_collection(line_segments)
+  # Turn off tick labels
+  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
+
+  (keypoint_locs, keypoint_edges,
+   edge_colors) = _keypoints_and_edges_for_display(
+       keypoints_with_scores, height, width)
+
+  line_segments.set_segments(keypoint_edges)
+  line_segments.set_color(edge_colors)
+  if keypoint_edges.shape[0]:
+    line_segments.set_segments(keypoint_edges)
+    line_segments.set_color(edge_colors)
+  if keypoint_locs.shape[0]:
+    scat.set_offsets(keypoint_locs)
+
+  if crop_region is not None:
+    xmin = max(crop_region['x_min'] * width, 0.0)
+    ymin = max(crop_region['y_min'] * height, 0.0)
+    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
+    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
+    rect = patches.Rectangle(
+        (xmin, ymin),rec_width,rec_height,
+        linewidth=1,edgecolor='b',facecolor='none')
+    ax.add_patch(rect)
+
+  fig.canvas.draw()
+  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+  image_from_plot = image_from_plot.reshape(
+      fig.canvas.get_width_height()[::-1] + (3,))
+  plt.close(fig)
+
+  if output_image_height is not None:
+    output_image_width = int(output_image_height / height * width)
+    image_from_plot = cv2.resize(
+        image_from_plot, dsize=(output_image_width, output_image_height),
+         interpolation=cv2.INTER_CUBIC)
+  return image_from_plot
+
+def visualize_keypoints(image, keypoints, dim_size=256):
+  """Visualizes the keypoints on the image."""
+  display_image = tf.expand_dims(image, axis=0)
+  display_image = tf.cast(tf.image.resize_with_pad(
+      display_image, dim_size, dim_size), dtype=tf.int32)
+  output_overlay = draw_prediction_on_image(np.squeeze(display_image.numpy(), axis=0), keypoints)
+  plt.figure(figsize=(5, 5))
+  plt.imshow(output_overlay)
+  plt.savefig(os.path.join('./test/images/', datetime.now().strftime('%Y%m-%d%H-%M%S')))
+  _ = plt.axis('off')
+
+def movenet_video_inference(mp4_path:str, num_of_frames_you_want:int=20):
   """Inference on a video file."""
   cap = cv2.VideoCapture(mp4_path)
   model = load_movenet_model()
-  while cap.isOpened():
+  dim_size = 256
+
+  total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+  step_size = total_frames // num_of_frames_you_want
+  frame_count = 0
+
+  while True:
     ret, frame = cap.read()
-    if not ret:
-      print("Can't receive frame (stream end?). Exiting ...")
+    if ret:
+      if frame_count % step_size == 0:
+        x = resize_image(frame, dim_size=dim_size)
+        image = tf.image.resize_with_pad(frame, dim_size, dim_size)
+
+        keypoints = movenet_predict(model, x)
+        visualize_keypoints(image, keypoints)
+      frame_count += 1
+    else:
       break
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = tf.expand_dims(frame, axis=0)
-    frame = tf.cast(tf.image.resize_with_pad(
-        frame, 256, 256), dtype=tf.int32)
-    frame = tf.cast(frame, dtype=tf.float32)
-    frame = frame / 255.0
-    keypoints = movenet_predict(model, frame)
-    left_angle, right_angle = calculate_2D_joint_angles(keypoints)
-    print(left_angle, right_angle)
-    output_overlay = draw_prediction_on_image(np.squeeze(frame.numpy(), axis=0), keypoints)
-    cv2.imshow('frame', output_overlay)
-    if cv2.waitKey(1) == ord('q'):
-      break
+
+    # Display the frame
+    cv2.imshow('Video', frame)
+    cv2.waitKey(1)
+  
   cap.release()
   cv2.destroyAllWindows()
+
+def visualize_movenet_models(model_list:list, input_sizes:list):
+  """"""
+  base = "/mnt/c/Users/owner/Software/couro-models/base/"
+  for idx, model_path in enumerate(model_list):
+    dim_size = input_sizes[idx]
+    image = load_local_test_image()
+    x = resize_image(image, dim_size=dim_size)
+    image = tf.image.resize_with_pad(image, dim_size, dim_size)
+    
+    model = load_movenet_model(os.path.join(base, model_path))
+    keypoints = movenet_predict(model, x)
+    visualize_keypoints(image, keypoints)
 
 if __name__ == "__main__":
 
